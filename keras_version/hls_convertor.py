8aa17db110ba469160892bd64869fdbf12fe3c32
Luke Johnson <luke.johnson@cern.ch>
2024-07-19 13:20:00 +0000

    updated hls convertor, updated hls environment, trained keras model on strip

--- keras_version/hls_convertor.py
+++ keras_version/hls_convertor.py
@@ -1,12 +1,5 @@
 import numpy as np
-from utils import predict as q_predict
-from utils import prepare_dataset
-from make_plots import get_accuracy, get_efficiency, get_bkg_score, bkg_rates
-from sklearn.metrics import confusion_matrix, log_loss, roc_curve, auc
-from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
-from tensorflow import keras
-import tensorflow as tf
 import pickle
 import shutil
 import hls4ml
@@ -14,33 +7,42 @@ from hls4ml.model.profiling import numerical
 from hls4ml.converters import keras_to_hls
 import os
 import json
-import yaml
-from qkeras.utils import _add_supported_quantized_objects
 from tensorflow.keras.models import load_model
 import textwrap
 import subprocess
-import seaborn as sns
 import sys
-import re
 
 
-def load_qkeras_model(path, modeltype):
-
+def load_keras_model(path):
+  '''
+  This function loads a Keras model from the specified path using 
+  TensorFlow's load_model function.
+  '''
   print ("Loading model")
-  qkeras_model = load_model(f"{path}/{modeltype}")
+  model = load_model(path)
   print ("Done")
-  return qkeras_model
+  return model
 
 
-def make_executable(path):
-    """Make a local file executable."""
-    mode = os.stat(path).st_mode
-    mode |= (mode & 0o444) >> 2  
-    os.chmod(path, mode)
+def make_executable(path): #Make a local file executable
+    '''
+    This function makes a local file executable by changing its permissions    
+    '''
+    # Get the current permissions of the file
+    mode = os.stat(path).st_mode 
+    
+    # Add execute permissions to the file
+    mode |= (mode & 0o444) >> 2  # Extracts the read permissions 
+                                 # >> 2 shifts these read permissions two bits to the right, effectively converting read to execute permissions
+                                 # mode |= ... uses the bitwise OR operator to add these execute permissions to the file's current mode
+    
+    os.chmod(path, mode)         # Sets the file’s permissions to the new mode, which now includes execute permissions.
+
 
 def shell_source(script):
-
-    
+    '''
+    This function sources a shell script and updates the environment variables
+    '''
     pipe = subprocess.Popen(". %s && env -0" % script, stdout=subprocess.PIPE, shell=True)
     output = pipe.communicate()[0].decode('utf-8')
     output = output[:-1] # fix for index out for range in 'env[ line[0] ] = line[1]'
@@ -54,7 +56,13 @@ def shell_source(script):
 
     os.environ.update(env)
 
+
 def vivado_setup(outdir):
+      '''
+      This function sets up the environment for Vivado HLS
+      It writes and executes a shell script to set up the necessary paths and environment variables
+      '''
+
       executable_text = textwrap.dedent(
       f"""
       export PATH=/opt/cactus/bin/uhal/tools:$PATH 
@@ -79,8 +87,19 @@ def vivado_setup(outdir):
       csim = synth = cosim = validation = export = vsynth = reset =1
     #   os.system(f"cd {outdir} && vivado_hls -f build_prj.tcl reset={reset} validation={validation} export={export} csim={csim} synth={synth} cosim={cosim}  export=True vsynth={vsynth}")
 
-def hls4ml_converter(params, model_type, model_path, outdir):
-    model = load_qkeras_model(model_path, model_type) 
+
+def hls4ml_converter(params, model_path, outdir):
+    '''
+    This function converts a Keras model to an HLS model using hls4ml. It:
+
+    Loads the Keras model.
+    Generates an HLS configuration from the Keras model.
+    Adjusts the configuration for specific layers.
+    Converts the Keras model to an HLS model.
+    Compiles the HLS model.
+    '''
+
+    model = load_keras_model(model_path) 
     print(model.summary())      
 
     #hls4ml.model.optimizer.OutputRoundingSaturationMode.layers = ['Activation']
@@ -104,19 +123,7 @@ def hls4ml_converter(params, model_type, model_path, outdir):
         config['LayerName'][l]['Precision']['bias'] = 'ap_fixed<8,2, AP_RND, AP_SAT>'
         config['LayerName'][l]['Trace'] = True
 
-    ## Set specific layers to custom quantizations
-        
-    # config['LayerName']['dense2']['Precision']['result'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-        
-    # config['LayerName']['relu_3']['Precision'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-        
-    # config['LayerName']['dense2']['Precision']['result'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-    # config['LayerName']['dense2']['Precision']['weight'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-    # config['LayerName']['dense2']['Precision']['bias'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-
-    # config['LayerName']['relu_4']['Precision']["result"] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-    # config['LayerName']['relu_4']['Precision']['weight'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-    # config['LayerName']['relu_4']['Precision']['bias'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
+    ## Can also set specific layers to custom quantizations, e.g:
 
     config['LayerName']['output_logit']['Precision']["result"] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
     config['LayerName']['output_logit']['Precision']['weight'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
@@ -129,7 +136,6 @@ def hls4ml_converter(params, model_type, model_path, outdir):
     config['LayerName']['final_sigmoid']['Precision']["result"] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
     config['LayerName']['final_sigmoid']['Precision']['weight'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
     config['LayerName']['final_sigmoid']['Precision']['bias'] = 'ap_fixed<16,6, AP_RND, AP_SAT>'
-    # config['LayerName']['final_sigmoid']['table_t'] = 'ap_fixed<64,1>'
 
     ## Parallelize convolutional layers
     config['LayerName']['conv2d_0']['ParallelizationFactor'] = 6
@@ -153,40 +159,14 @@ def hls4ml_converter(params, model_type, model_path, outdir):
                                                            output_dir=outdir,    
                                                            clock_period=1000./params['clock_freq'], 
                                                            part=params['part'],
-                                                           io_type='io_parallel')     
+                                                           io_type=params["io_type"])     
     
     hls_model.compile()
+
     return model, hls_model
 
-def prepare_dataset(dataset, numsig, numbkg):
 
-    f=open(dataset, 'rb')
-    data = pickle.load(f) 
-    y = data["labels"]
-    sig_idx = data['idx'][(y==1)]
-    sig_x = data['images'][(y==1)]
-    sig_y = data['labels'][(y==1)]
-    sig_ht = data[b'phaseIPFJetHt'][(y==1)]
-    bkg_idx = data['idx'][(y==0)]
-    bkg_x = data['images'][(y==0)]
-    bkg_y = data['labels'][(y==0)]
-    bkg_ht = data[b'phaseIPFJetHt'][(y==0)]
-    idx = np.concatenate((sig_idx[:numsig], bkg_idx[:numbkg]))
-    x = np.concatenate((sig_x[:numsig], bkg_x[:numbkg]))
-    y = np.concatenate((sig_y[:numsig], bkg_y[:numbkg]))
-    ht = np.concatenate((sig_ht[:numsig], bkg_ht[:numbkg]))
-
-    #ht = np.log10(ht+1)
-    #ht = ht / np.max(ht)
-    x = tf.expand_dims(x, -1)
-    y = tf.expand_dims(y, -1)
-    ht = tf.expand_dims(ht, -1)
-    outdata = {'x': x, 'ht': ht, 'y': y}
-
-    return outdata
-
-
-
+### Ignore this for now
 def layer_plots(model, hls_model, X_test, y_test, outdir):
     X_test = np.array(X_test)
     y_test = np.array(y_test).flatten()
@@ -242,136 +222,37 @@ def layer_plots(model, hls_model, X_test, y_test, outdir):
             plt.clf()
 
 
-def plot_rocs(y_red, q_scores, hls_scores, gdir):
-
-    bgr = (40*10**3) * (2760 / 3564)
-    fpr, tpr, thresholds, aucs = {}, {}, {}, {}
-    fpr['QKeras'], tpr['QKeras'], thresholds['QKeras'] = roc_curve(list(map(int, list(y_red))), q_scores) #hacky but roc-curve object expects very specific dtype only
-    fpr['HLS'], tpr['HLS'], thresholds['HLS'] = roc_curve(list(map(int, list(y_red))), hls_scores) #hacky but roc-curve object expects very specific dtype only
-    for model in fpr.keys():
-        aucs[model] = auc(fpr[model], tpr[model])
-
-    rscores = np.random.random(q_scores.shape[0])
-    rfpr, rtpr, rthresholds = roc_curve(list(map(int, list(y_red))), rscores)
-    rfpr *= (40*10**3) * (2760 / 3564)
-
-    plt.figure(facecolor='w')
-    #plt.plot(rfpr,rtpr,label='Random',linestyle='-', color='gray', alpha=0.5, linewidth=2.0)
-    for  i, model in enumerate( fpr.keys() ):
-        fpr[model]*=bgr
-        plt.plot(fpr[model],tpr[model],label='%s tagger, AUC = %.1f%%'%(model,aucs[model]*100.),linestyle='-')
-    plt.xlabel("Background Rate [kHz]")
-    plt.ylabel("Signal Efficiency")
-    plt.xlim(0.1,32000)
-    plt.semilogx()
-    plt.grid(True)
-    plt.legend(loc='upper left')
-    plt.figtext(0.25, 0.90,'hls4ml',fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
-    plt.savefig(f'{gdir}/rocs.jpg', dpi=300)
-    plt.show()
-    plt.clf()
-    
-    q_min_thr = thresholds['QKeras'][fpr['QKeras']<10][-1]
-    hls_min_thr = thresholds['HLS'][fpr['HLS']<10][-1]
-    q_eff = tpr['QKeras'][fpr['QKeras']==q_min_thr]
-    hls_eff = tpr['HLS'][fpr['HLS']==hls_min_thr]
-
-    return q_eff, q_min_thr, hls_eff, hls_min_thr
-
-
-def plot_scores(q_scores, hls_scores, gdir):
-    plt.figure(facecolor='w')
-    plt.scatter(q_scores, hls_scores, s=1)
-    plt.xlabel('QKeras Model predictions')
-    plt.ylabel('HLS model predictions')
-    plt.xlim(0,1)
-    plt.ylim(0,1)
-    plt.savefig(f'{gdir}/qmodel_vs_hls.jpg', dpi=200)
-
-
-def capture_and_parse_vivado_report(params, outdir, report_file_path):
-    # Function to capture Vivado output to a file
-    def capture_vivado_output_to_file():
-        original_stdout = sys.stdout  # Save a reference to the original standard output
-
-        with open(report_file_path, 'w') as f:
-            sys.stdout = f  # Change the standard output to the file we created.
-            try:
-                if params['read_vivado']:
-                    hls4ml.report.read_vivado_report(outdir)
-            finally:
-                sys.stdout = original_stdout  # Reset the standard output to its original value
-
-
-
-    def parse_vivado_report(report_text):
-        # Patterns to extract the utilization summary and latency information
-        utilization_pattern = r'Utilization \(%\)\s*\|([^|]*)\|([^|]*)\|([^|]*)\|([^|]*)\|([^|]*)\|'
-        # Updated latency pattern to match the new format
-        latency_pattern =  r"\|\s*([0-9]+)\s*\|\s*([0-9]+)\s*\|\s*([\d.]+)\s*us\s*\|\s*([\d.]+)\s*us\s*\|\s*([0-9]+)\s*\|\s*([0-9]+)"
-
-        data = {}
-        # Search and extract utilization data
-        utilization_match = re.search(utilization_pattern, report_text, re.MULTILINE)
-        if utilization_match:
-            # Extracting and assigning utilization percentages directly
-            data['BRAM_18K'] = utilization_match.group(1).strip()
-            data['DSP48E'] = utilization_match.group(2).strip()
-            data['FF'] = utilization_match.group(3).strip()
-            data['LUT'] = utilization_match.group(4).strip()
-            data['URAM'] = utilization_match.group(5).strip()
-
-        # Search and extract latency data
-        latency_match = re.search(latency_pattern, report_text, re.MULTILINE)
-        if latency_match:
-            # Assigning latency data based on the captured groups
-            data['Latency Cycles Min'] = latency_match.group(1).strip()
-            data['Latency Cycles Max'] = latency_match.group(2).strip()
-            data['Latency Min (us)'] = latency_match.group(3).strip()
-            data['Latency Max (us)'] = latency_match.group(4).strip()
-            data['Interval Min'] = latency_match.group(5).strip()
-            data['Interval Max'] = latency_match.group(6).strip()
-
-        return data
-
-
-    # Step 1: Capture the Vivado output to a file
-    capture_vivado_output_to_file()
-
-    # Step 2: Read the captured report from the file
-    with open(report_file_path, 'r') as file:
-        report_text = file.read()
-
-    # Step 3: Parse the report text to extract information
-    extracted_data = parse_vivado_report(report_text)
-
-    return extracted_data
-
-
 
 def main():
-    params = yaml.safe_load(open('params.yaml'))['hls_convertor']
-    model_type = "QKerasModel"
-    MODELPATH = "/software/ek19824/ML_software/cmsml_l1_software/qk_strides/20240521--113428-best-s3f1"
-    #bits = yaml.safe_load(open('params.yaml'))['train_models']['bitwidth'][1:-1]
-    #test_path = "/storage/2/ek19824/ML_data/231124_alldata/dataset_testing.pickle"
-    test_path = '/storage/2/ek19824/ML_data/240213_all18data/dataset_testing.pickle'
-    # test_path = '/storage/2/ek19824/ML_data/230521_all24data/dataset_testing.pickle'
-    # test_path = '/storage/2/ek19824/ML_data/240521_all42data/dataset_testing.pickle'
+    '''
+    Main function:
 
-    outdir = f"{MODELPATH}/hls_converted"
+    Defines the parameters for the HLS conversion.
+    Specifies the paths for the model and output directory.
+    Sets up the Vivado HLS environment.
+    Calls the hls4ml_converter function to perform the conversion.
+    Builds the HLS model.
+    Prints and saves the HLS configuration.
+    '''
+    params = {"clock_freq": 360,
+              "part": 'xcvu9p-flga2577-2-e',
+              "ReuseFactor": 1,
+              "io_type": "io_serial"
+    }
+    MODELPATH = "trained_models_lite/reduced_constkernels/model_epoch_9.h5"            ### Add model path here
 
-    gdir = f'{outdir}/graphs'
+    outdir = "hls_outputs"         ### Set to where you want to output the model
+
     if os.path.exists(outdir):
         shutil.rmtree(outdir)
     os.makedirs(outdir, exist_ok=False)
-    os.makedirs(gdir, exist_ok=False)
     vivado_setup(outdir)
-    q_model, hls_model = hls4ml_converter(params, model_type, MODELPATH, outdir)
-    # hls_model.build(csim=True)        # Uncomment line if you want to perform full C-synthesis to get resource usage as well as producing plots
+
+    model, hls_model = hls4ml_converter(params, MODELPATH, outdir)
+    hls_model.build(csim=True)        # Uncomment line if you want to perform full C-synthesis to get resource usage
 
     hls_config = hls_model.config.config["HLSConfig"]
-    # print(hls_config)
+
     ## Print and save HLS config
     for i in hls_config["LayerName"].keys():
         print(f"{i:<20}: {hls_config['LayerName'][i]}")
@@ -380,51 +261,29 @@ def main():
         json.dump(hls_config, outfile)
     print("-----------------------------------")  
 
-    # Prepare dataset and compare predictions scores
-    NUMSIG = 20000
-    NUMBKG = 400000
-    test_data = prepare_dataset(test_path, NUMSIG, NUMBKG)
-    X = test_data['x']
-    y = test_data['y']
-    print('Test images: ', len(X), len(y))
-    q_scores, q_model = q_predict(f"{MODELPATH}/{model_type}", X, verbose=True)
-    hls_scores = np.array(hls_model.predict(np.ascontiguousarray(X)))
-    hls_preds = {"score": np.array(hls_scores),
-                 "label": np.array(y)}
-    np.save(f"{outdir}/pred_labels.npy", y)
-    # np.save(f"{outdir}/predictions_hls.npy", hls_preds)
-    np.save(f"{outdir}/predictions.npy", q_scores)
-    with open(f'{outdir}/predictions_hls.pickle', 'wb') as f:
-        pickle.dump(hls_preds, f)
-        f.close()
-    
-    # Compare ROC curves for HLS and Keras models
-    q_eff, q_min_thr, hls_eff, hls_min_thr = plot_rocs(y, q_scores, hls_scores, gdir)
-    # print(f'Thresholds | QK: {q_min_thr} | HLS: {hls_min_thr}')
-    # print(f'Efficiency | QK: {q_eff} | HLS: {hls_eff}')
+
+    ### Add here some code to load in some data, add noise, and perform inference. Try to do this for both the model and hls_model.
+    ### I think you can call the hls model in the same way you would with the normal model.
+
+
+
+
+
+    ### Ignore this for now
     
     # Plot weight and activation distribution bo plots
-    p1,p2,p3,p4 = numerical(model=q_model, hls_model=hls_model, X=X)
-    x=0
-    for fig in [p1,p2,p3,p4]:
-        x+=1
-        fig.savefig(f'{gdir}/weights_plot{x}.jpg')
-        fig.show()
-    plt.clf()
+    # p1,p2,p3,p4 = numerical(model=model, hls_model=hls_model, X=X)
+    # x=0
+    # for fig in [p1,p2,p3,p4]:
+    #     x+=1
+    #     fig.savefig(f'{gdir}/weights_plot{x}.jpg')
+    #     fig.show()
+    # plt.clf()
 
     # Plot activation comparison and distribution for each layer
-    layer_plots(q_model, hls_model, X, y, gdir)
+    # layer_plots(model, hls_model, X, y, gdir)
 
-    # report_file_path = f'{outdir}/report.txt'
-    # # Example usage
-    # extracted_data = capture_and_parse_vivado_report(params, outdir, report_file_path)
-    
-    # file_path = f'{gdir}/stats.txt'
 
-    # # Open the file in write mode and write the output
-    # with open(file_path, 'w') as file:
-    #     file.write(f'Efficiency: {hls_eff}\n')
-    #     file.write(f'hardware {str(list(extracted_data.items()))}\n')
     print(MODELPATH)
     print("------ HLS SYNTHESIS COMPLETE -----")
 
